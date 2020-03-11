27405f67fe9312b4d99cd5c06311b35a514d5934
Bohdan Forostianyi <qwebeck5@gmail.com>
2020-03-11 21:52:14 +0100

    Changed long function with if on geting value from map

--- Vass-2/src/headers/shortcut_v.h
+++ Vass-2/src/headers/shortcut_v.h
@@ -14,19 +14,27 @@
 #include <QDebug>
 #include "headers/saver.h"
 #include "headers/shortcut.h"
+#include <map>
 
-enum class State{Ranged,Single};
+enum class State
+{
+    Ranged,
+    Single
+};
 class shortcut_v
 {
 private:
-    static shortcut_v * Shortcuts_Base;
+    static shortcut_v *Shortcuts_Base;
     shortcut_v();
+    static const std::map<std::string, int> _keymapping;
 
 public:
     ~shortcut_v();
-//singleton class
-    static shortcut_v * Initialialize(){
-        if(Shortcuts_Base == nullptr)Shortcuts_Base = new shortcut_v;
+    //singleton class
+    static shortcut_v *Initialialize()
+    {
+        if (Shortcuts_Base == nullptr)
+            Shortcuts_Base = new shortcut_v;
         return Shortcuts_Base;
     }
 
@@ -41,8 +49,8 @@ public:
     std::vector<int> _hotKeys;
     QString _shortcuts_file;
     std::regex _app_parser{R"((.*)\/(.*)(\..*)$)"};
-    std::vector<std::string> _image_ext{".png",".jpg",".bmp",".svg",".ico"};
-    std::vector<std::string> _doc_ext{".pdf",".doc",".lib",".csv",".odt"};
+    std::vector<std::string> _image_ext{".png", ".jpg", ".bmp", ".svg", ".ico"};
+    std::vector<std::string> _doc_ext{".pdf", ".doc", ".lib", ".csv", ".odt"};
     long int _summirized_weight; // sum of weighted elements in shortcuts
     void parse_names(QString);
     void writeToFile();
@@ -50,7 +58,6 @@ public:
     int return_key_code(QString);
     int return_modifier(QString);
     std::string setModPrefix(unsigned int modifier);
-
 };
 
 #endif // SHORTCUT_V_H
